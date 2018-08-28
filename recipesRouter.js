d0438c57c4f75bcf9e3abb15b7c16be97a3b92e1
Adriana Suarez <agsmq@yahoo.com>
2018-08-28 11:09:54 -0500

    Fixed put test for recipes

--- recipesRouter.js
+++ recipesRouter.js
@@ -70,12 +70,12 @@ router.put("/:id", (req, res) => {
     return res.status(400).send(message);
   }
   console.log(`Updating recipe \`${req.params.id}\``);
-  Recipes.update({
+  const post = Recipes.update({
     id: req.params.id,
     name: req.body.name,
     ingredients: req.body.ingredients
   });
-  res.status(204).end();
+  res.status(201).send(post);
 });
 
 module.exports = router;
